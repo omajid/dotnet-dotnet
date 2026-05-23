8d28caffbb2d7086a0bcb92e9cd6c37f3d19b34a
Omair Majid <omajid@redhat.com>
2026-05-23 11:15:11 -0400

    Use deterministic order when creating nupkg hash

--- src/nuget-client/src/NuGet.Core/NuGet.Packaging/PackageCreation/Authoring/PackageBuilder.cs
+++ src/nuget-client/src/NuGet.Core/NuGet.Packaging/PackageCreation/Authoring/PackageBuilder.cs
@@ -375,7 +375,7 @@ namespace NuGet.Packaging
             {
                 using (var hashFunc = new Sha512HashFunction())
                 {
-                    foreach (var file in Files)
+                    foreach (var file in Files.OrderBy(f => f, new NormalizedPathComparer()))
                     {
                         var data = ReadAllBytes(file.GetStream());
                         hashFunc.Update(data, 0, data.Length);
